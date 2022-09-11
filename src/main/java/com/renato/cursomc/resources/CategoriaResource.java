866a533b0b20e03ac69f96b98317b534df6546a1
RenatoL <renatol1857@gmail.com>
2022-09-11 11:53:07 -0300

    Implantei FindById and FindAll

--- src/main/java/com/renato/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/renato/cursomc/resources/CategoriaResource.java
@@ -2,27 +2,38 @@ package com.renato.cursomc.resources;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renato.cursomc.domain.Categoria;
+import com.renato.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria("Informatica");
-		Categoria cat2 = new Categoria("Escritorio");
-		
-		List<Categoria> lstCat = new ArrayList<>();
-		lstCat.add(cat1);
-		lstCat.add(cat2);		
-		
-		return lstCat; 
+		return categoriaService.listar(); 
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> buscarPor(@PathVariable("id") Integer id) {
+		Optional<Categoria> obj = categoriaService.buscarPor(id);
+		if (obj.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Categodia ID " + id + " nao encontrada.");
+		return ResponseEntity.status(HttpStatus.OK).body(obj);
 	}
 
 }
---
+++ src/main/java/com/renato/cursomc/services/CategoriaService.java
@@ -0,0 +1,32 @@
+package com.renato.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renato.cursomc.domain.Categoria;
+import com.renato.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Optional<Categoria> buscarPor(Integer id) {
+		return categoriaRepository.findById(id);
+	}
+
+	public Categoria salvar(Categoria cat) {
+		return categoriaRepository.save(cat);
+	}
+	
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+	
+	
+}
